8a53c04c8687d5593f7a7ca7af58ce99729ed4cb
Marina Ramalhete <marinaramalhete@outlook.com>
2022-05-21 20:17:40 -0300

    Add chapter 02

---
+++ 02_selection_sort/selection_sort.py
@@ -0,0 +1,23 @@
+def find_smallest(arr):
+    """Return the smallest value in an array."""
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(arr):
+    """Return sort array."""
+    new_arr = []
+
+    for _ in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+print(selection_sort([5, 3, 6, 2, 10]))
